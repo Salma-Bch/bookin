caf04a3b4fcb524dbcadd83882db9392343ea8dd
MOUNCIF <Mons-L@users.noreply.github.com>
2021-02-25 14:04:31 +0100

    Création des classe dao pour purchase

---
+++ www/class/dao/object/PurchaseDaoImpl.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace dao\object;
+
+
+class PurchaseDaoImpl implements PurchaseDao
+{
+
+}
---
+++ www/class/model/Purchase.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace model;
+
+
+class Purchase
+{
+    private int $clientId;
+    private int $bookId;
+}
