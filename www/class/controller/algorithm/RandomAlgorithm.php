d918374e92f8e59ded3af17797bff048ac30bd04
farah <farah-manoubi@users.noreply.github.com>
2021-03-13 15:45:46 +0100

    Random algo

--- www/class/controller/algorithm/RandomAlgorithm.php
+++ www/class/controller/algorithm/RandomAlgorithm.php
@@ -4,10 +4,10 @@ namespace controller;
 
 use dao\DAOFactory;
 use model\Client;
+use utility\Math;
 
 class RandomAlgorithm {
 
-    private Client $client;
     private array $books;
 
     /**
@@ -17,25 +17,20 @@ class RandomAlgorithm {
     }
 
     /**
-     * Retourne tout les livres de la base de données.
+     * Retourne des livres aléatoirement.
      *
+     * @param int $nbrOfBook
      * @return array
      */
-    public function getAllBooks():array{
-        //Récupere la liste de tous les livres
-        $daoFactory = DAOFactory::getInstance();
-        $booksDao = $daoFactory->getBookDao();
-        $books = $booksDao->getAll();
+    public function suggest(int $nbrOfBook):array{
+        $booksToReturn = array();
+        $books = $this->books;
 
-        //Faire un tableau de nombre aleatoire de nbQuonVeut allant de 0 à count($books)-1
-
-
-        //Récuperer les livres a retourner dans un array et les retourner
-        $booksReturned = array();
-        foor(){
-
+        $randomKeys = array_rand($books, $nbrOfBook);
+        foreach($randomKeys as $randomKey){
+            array_push($booksToReturn, $books[$randomKey]);
         }
-        return $booksReturned;
+        return $booksToReturn;
     }
 
 }
