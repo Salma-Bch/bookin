55bd5a28d9d9ac2212a94456fa0781b8ed22c536
MOUNCIF <Mons-L@users.noreply.github.com>
2021-03-13 15:47:40 +0100

    Mise a jour de ContentAlgorithm.php et dde Suggestion.php

--- www/class/controller/Suggestion.php
+++ www/class/controller/Suggestion.php
@@ -27,84 +27,18 @@ class Suggestion {
 
     public function suggest():array{
         $booksToDisplay = array();
-        $contentTypeModelling = new ContentModel($this->client);
-        $categoryModel = $contentTypeModelling->getCategoryModel();
+
+        $contentAlgorithm = new ContentAlgorithm($this->books, $this->client);
 
        /* if($priceModel != -1) {
             $booksToDisplay = $this->priceBased($priceModel, $this->books, 5);
         }*/
 
-        $books = $this->categoryBased($categoryModel, $this->books, $contentTypeModelling);
+        $books = $contentAlgorithm->suggest();
         var_dump($books);
         return $booksToDisplay;
     }
 
-    public function categoryBased(array $categoryModel, array $books, ContentModel $contentTypeModelling){
-        $booksReturned = array();
-        // Si la categorie est a 0% je ne met aucun livre de cette dernière
 
-        foreach ($categoryModel as $category){
-            if($category != 0){
-                foreach ($books as $book){
-                    if($book->getCategoryName() == key($categoryModel))
-                        array_push($booksReturned, $book);
-                }
-                next($categoryModel);
-            }
-        }
-        $ageRangeModel = $contentTypeModelling->getAgeRangeModel();
-        $booksReturned = $this->ageRangeBased($ageRangeModel, $booksReturned, $contentTypeModelling);
-        return $booksReturned;
-    }
-
-    public function ageRangeBased(array $ageRangeModel, array $books, ContentModel $contentTypeModelling){
-        $booksReturned = array();
-        // Si la tranche d'age est a 0% je ne met aucun livre de cette dernière
-
-        foreach ($ageRangeModel as $ageRange){
-            if($ageRange != 0){
-                foreach ($books as $book){
-                    if($book->getAgeRange() == key($ageRangeModel))
-                        array_push($booksReturned, $book);
-                }
-                next($ageRangeModel);
-            }
-        }
-        $bookSizeModel = $contentTypeModelling->getBookSizeModel();
-        $booksReturned = $this->bookSizeBased($bookSizeModel, $booksReturned, $contentTypeModelling);
-        return $booksReturned;
-    }
-
-    public function bookSizeBased(array $bookSizeModel, array $books, ContentModel $contentTypeModelling){
-        $booksReturned = array();
-        // Si la tranche d'age est a 0% je ne met aucun livre de cette dernière
-
-        foreach ($bookSizeModel as $bookSize){
-            if($bookSize != 0){
-                foreach ($books as $book){
-                    if($book->getBookSize() == key($bookSizeModel))
-                        array_push($booksReturned, $book);
-                }
-                next($bookSizeModel);
-            }
-        }
-        $priceModel = $contentTypeModelling->getPriceModel();
-        $booksReturned = $this->priceBased($priceModel, $booksReturned, 5);
-        return $booksReturned;
-    }
-
-    public function priceBased(int $priceModel, array $books,int $selected):array{
-        $prices=array();
-        $booksReturned = array();
-        foreach ($books as $book){
-            array_push($prices, $book->getPrice());
-        }
-        $nearestPrices = Math::nearestFigure($priceModel, $prices, $selected);
-        foreach ($books as $book){
-            if(in_array($book->getPrice(), $nearestPrices))
-                array_push($booksReturned, $book);
-        }
-        return $booksReturned;
-    }
 
 }
--- www/class/controller/algorithm/ContentAlgorithm.php
+++ www/class/controller/algorithm/ContentAlgorithm.php
@@ -4,9 +4,94 @@
 namespace controller;
 
 
+use dao\DAOFactory;
 use model\Client;
+use utility\Math;
 
 class ContentAlgorithm {
     private Client $client;
     private array $books;
+    private ContentModel $contentModel;
+
+    public function __construct(array $books, Client $client) {
+        $this->books = $books;
+        $this->client = $client;
+
+    }
+
+    public function suggest():array{
+        $books = $this->categoryBased($this->books, $this->contentModel);
+        return $books;
+    }
+
+    public function categoryBased(array $books, ContentModel $contentModel){
+        $booksReturned = array();
+        // Si la categorie est a 0% je ne met aucun livre de cette dernière
+        $categoryModel = $contentModel->getCategoryModel();
+
+        foreach ($categoryModel as $category){
+            if($category != 0){
+                foreach ($books as $book){
+                    if($book->getCategoryName() == key($categoryModel))
+                        array_push($booksReturned, $book);
+                }
+                next($categoryModel);
+            }
+        }
+        $booksReturned = $this->ageRangeBased($booksReturned, $contentModel);
+        return $booksReturned;
+    }
+
+    public function ageRangeBased(array $books, ContentModel $contentModel){
+        $booksReturned = array();
+        // Si la tranche d'age est a 0% je ne met aucun livre de cette dernière
+        $ageRangeModel = $contentModel->getAgeRangeModel();
+
+        foreach ($ageRangeModel as $ageRange){
+            if($ageRange != 0){
+                foreach ($books as $book){
+                    if($book->getAgeRange() == key($ageRangeModel))
+                        array_push($booksReturned, $book);
+                }
+                next($ageRangeModel);
+            }
+        }
+        $bookSizeModel = $contentModel->getBookSizeModel();
+        $booksReturned = $this->bookSizeBased($booksReturned, $contentModel);
+        return $booksReturned;
+    }
+
+    public function bookSizeBased(array $books, ContentModel $contentModel){
+        $booksReturned = array();
+        // Si la tranche d'age est a 0% je ne met aucun livre de cette dernière
+        $bookSizeModel = $contentModel->getBookSizeModel();
+
+        foreach ($bookSizeModel as $bookSize){
+            if($bookSize != 0){
+                foreach ($books as $book){
+                    if($book->getBookSize() == key($bookSizeModel))
+                        array_push($booksReturned, $book);
+                }
+                next($bookSizeModel);
+            }
+        }
+        $priceModel = $contentModel->getPriceModel();
+        $booksReturned = $this->priceBased($booksReturned, 5, $contentModel);
+        return $booksReturned;
+    }
+
+    public function priceBased(array $books,int $selected, ContentModel $contentModel):array{
+        $prices=array();
+        $booksReturned = array();
+        $priceModel = $contentModel->getPriceModel();
+        foreach ($books as $book){
+            array_push($prices, $book->getPrice());
+        }
+        $nearestPrices = Math::nearestFigure($priceModel, $prices, $selected);
+        foreach ($books as $book){
+            if(in_array($book->getPrice(), $nearestPrices))
+                array_push($booksReturned, $book);
+        }
+        return $booksReturned;
+    }
 }
